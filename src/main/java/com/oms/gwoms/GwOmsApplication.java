cd990f44bd1dcc9e0c567100fa6035bae522d447
Elkhan Isayev <is.elxan@gmail.com>
2025-01-17 09:11:16 +0400

    first init

---
+++ src/main/java/com/oms/gwoms/GwOmsApplication.java
@@ -0,0 +1,13 @@
+package com.oms.gwoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwOmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GwOmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oms/gwoms/GwOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oms.gwoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
